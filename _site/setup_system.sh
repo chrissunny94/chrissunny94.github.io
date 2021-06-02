8c61063e1211eb7fabf04ad82a830941cd12beb2
chrissunny94 <chrissunny94@gmail.com>
2021-06-02 18:49:17 +0530

    New changes

--- _site/setup_system.sh
+++ _site/setup_system.sh
@@ -1,4 +1,7 @@
 sudo apt-get install ruby-full build-essential zlib1g-dev
+mkdir ~/.ruby
+echo 'export GEM_HOME=~/.ruby/' >> ~/.bashrc
+echo 'export PATH="$PATH:~/.ruby/bin"' >> ~/.bashrc
 echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
